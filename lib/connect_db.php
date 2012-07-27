c3198f2be7d27c9a087fb6a98c4a5c22a20e7f81
pap-x <chrspapadop@gmail.com>
2012-07-27 16:42:09 +0300

    fixed

--- lib/connect_db.php
+++ lib/connect_db.php
@@ -1,13 +1,13 @@
-<?php
+<?
 	/* Ta stoixeia tis vasis apo to hosting*/
 	$mysql_host = "localhost";
 	$mysql_database = "ethmmy_db";
 	$mysql_user = "thmmy";
-	$mysql_password = "oaR9bdXMeOM4rw9SYAx";
-	
+	$mysql_password = "oaR9bdXMeOM4rw9SYAxG";
+
 	/*kane tin syndesi kai 'epelexe' tin vasi*/
-	$conn = mysql_connect($mysql_host, $mysql_user, $mysql_password) or die("fed");
-	mysql_select_db($mysql_database, $conn) or die("fed2");
+	$conn = mysql_connect($mysql_host, $mysql_user, $mysql_password) or die(mysql_error());
+	mysql_select_db($mysql_database, $conn) or die(mysql_error());
 
 	$query = "SET NAMES 'utf8'";
 	mysql_query($query);
