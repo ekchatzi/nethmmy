ed2af349c7f0b8f9c951a1b3da5d404f56eeb5df
pap-x <chrspapadop@gmail.com>
2012-08-11 23:06:00 +0300

    fixed navigation

--- views/navigation.php
+++ views/navigation.php
@@ -2,24 +2,38 @@
 <?php
 	$query = "SELECT * FROM classes";
 	$ret = mysql_query($query);
-	if($ret && mysql_num_rows($ret)) {
-		while($row = mysql_fetch_array($ret)) {
-			echo "<li><a href='class/".$row['id']."/'>".$row['title']."</a>
-			<ul class='navigationClassDirectories' id='class".$row['id']."'>
-				<li><a href='announcements/".$row['id']."/'>"._("Announcements")."</a></li>
-				<li><a href='class_files/".$row['id']."/'>"._("Files")."</a></li>
-				</ul></li>";
+	if($ret && mysql_num_rows($ret)) 
+	{
+		while($row = mysql_fetch_array($ret)) 
+		{
+			echo "<li><a href='class/".$row['id']."/'>".$row['title']."</a>";
+			if (isset($_GET['id'])&&$_GET['id']>0) 
+			{
+				if ($_GET['id']==$row['id']) 
+				{	
+					$view_names = array('announcements' => _('Announcements'), 'class_files' => _('Files'));
+					echo "<ul class='navigationClassDirectories'>";
+					foreach ($view_names as $view => $view_title) 
+					{
+						echo "<li ";
+						if (isset($_GET['v'])) 
+						{
+							if ($_GET['v']==$view) 
+							{
+								echo "id='navigationHl'";
+							}
+						}
+						echo "><a href=".$view."/".$row['id']."/>".$view_title."</a></li>";	
+					}	
+					echo "</ul></li>";
+				}
+			}
 		}
+		echo "<li><a href='classes/'>"._("All Classes")."</a>";
 	}
-	else {
+	else 
+	{
 		echo "<li>"._("No classes there are")."</li>";
 	}
 ?>
 </ul>
-<script>
-	var cid = <?php echo isset($_GET['id'])?$_GET['id']:0;?>;
-	if (cid > 0) {
-		var select = 'class'+cid;
-		$('#'+select).show('fast');
-	}
-</script>
